70cad503de099d9e127ef81c5294473c2685db5c
Ajani Motta <amotta11003@yahoo.com>
2020-02-10 16:22:23 -0500

    heroku config vars

--- routes/auth.js
+++ routes/auth.js
@@ -37,11 +37,11 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res) {
     console.log("LOGGING IN (SERVER SIDE)");
     console.log("CLIENT ID: ", spotifyApi._credentials.client_id);
-    console.log("CLIENT ID: ", config.spotifyClientID);
+    console.log("CLIENT ID: ", process.env.SPOTIFY_CLIENT_ID);
     console.log("CLIENT SECRET: ", spotifyApi._credentials.client_secret);
-    console.log("CLIENT SECRET: ", config.spotifyClientSecret);
+    console.log("CLIENT SECRET: ", process.env.SPOTIFY_CLIENT_SECRET);
     console.log("REDIRECT URI: ", spotifyApi._credentials.redirect_uri);
-    console.log("REDIRECT URI: ", config.redirectURI);
+    console.log("REDIRECT URI: ", process.env.REDIRECT_URI);
     var state = generateRandomString(16);
     res.cookie(stateKey, state);
   
