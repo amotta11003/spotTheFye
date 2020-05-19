b3e3b9cd4b3f3309ef9e9216856827478212b904
Ajani Motta <amotta11003@yahoo.com>
2020-05-19 14:39:13 -0400

    handle artists with no images

--- spotify-react/src/components/TopStreamedArtist/index.js
+++ spotify-react/src/components/TopStreamedArtist/index.js
@@ -4,25 +4,50 @@ import { withStyles } from "@material-ui/core/styles";
 import tableStyle from '../../assets/jss/material-kit-react/views/table';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import MusicNoteTwoToneIcon from '@material-ui/icons/MusicNoteTwoTone';
 
-const TopStreamedArtist = ({artist, rank, classes}) => (
-    <TableRow>
-        <TableCell><h3 className={classes.title}>{rank}</h3></TableCell>
-        <TableCell>
-            <a href={artist.uri}>
-                <span>
-            
-                </span>
-                <h5 className={classes.cardTitle}>{artist.name}</h5>
-            </a>
-        </TableCell>
-        <TableCell>
-            <p className={classes.title}>{artist
-                .genres
-                .join(', ')}</p>
-        </TableCell>
-        <TableCell><p className={classes.title}>{artist.popularity}</p></TableCell>
-    </TableRow>
-);
+const TopStreamedArtist = ({artist, rank, classes}) => {
+    if (artist.images.length < 3){
+        return (
+            <TableRow>
+                <TableCell><h3 className={classes.title}>{rank}</h3></TableCell>
+                <TableCell>
+                    <a href={artist.uri}>
+                        <span>
+                            <img src={artist.images[0].url} alt={artist.name} 
+                                className={classNames(classes.imgRaised,classes.imgRounded,classes.imgFluid)}/>
+                        </span>
+                        <h5 className={classes.cardTitle}>{artist.name}</h5>
+                    </a>
+                </TableCell>
+                <TableCell>
+                    <p className={classes.title}>{artist
+                        .genres
+                        .join(', ')}</p>
+                </TableCell>
+                <TableCell><p className={classes.title}>{artist.popularity}</p></TableCell>
+            </TableRow>
+        );
+    } else return (
+        <TableRow>
+            <TableCell><h3 className={classes.title}>{rank}</h3></TableCell>
+            <TableCell>
+                <a href={artist.uri}>
+                    <span>
+                        <MusicNoteTwoToneIcon/>
+                    </span>
+                    <h5 className={classes.cardTitle}>{artist.name}</h5>
+                </a>
+            </TableCell>
+            <TableCell>
+                <p className={classes.title}>{artist
+                    .genres
+                    .join(', ')}</p>
+            </TableCell>
+            <TableCell><p className={classes.title}>{artist.popularity}</p></TableCell>
+        </TableRow>
+    );
+};
+
 
 export default withStyles(tableStyle)(TopStreamedArtist);
